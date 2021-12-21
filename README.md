806b4e0cb6147c9e2f3ab8314c08266706e5ae1b
Dan Radion <danradion@google.com>
2021-12-21 00:33:32 -0800

    initial structure for scraper-service

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ A set of microservices that provide clients a way to look up metadata about comp
 
 This repo contains several loosely-coupled microservices. To develop on a particular microservice, `cd` into the corresponding folder.
 
-* `api/` - the entrypoint for external clients.
+- `api/` - the entrypoint for external clients.
+- `scraper-service/` - subsystem that provides on-demand scraping of company metadata.
---
+++ scraper-service/src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Scraper Service')
+    .setDescription('The scraper-service API description')
+    .setVersion('1.0')
+    .addTag('company')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const apiSubpath = 'api';
+  SwaggerModule.setup(apiSubpath, app, document);
+
+  await app.listen(3001, '0.0.0.0');
+
+  const url = await app.getUrl();
+
+  console.log(`Application is running on: ${url}`);
+  console.log(`See API: ${url}/${apiSubpath}`);
+}
+bootstrap();
