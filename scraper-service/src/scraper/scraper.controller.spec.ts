c2887703912e42a7cd6663ff8b0a8a9ac48ff815
Dan Radion <danradion@google.com>
2021-12-22 23:52:08 -0800

    begin defining a fetchByCompanyId endpoint

--- scraper-service/src/scraper/scraper.controller.spec.ts
+++ scraper-service/src/scraper/scraper.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { FetchByCompanyIdDto } from './dto/fetch.dto';
 import { ScraperController } from './scraper.controller';
 
 describe('ScraperController', () => {
@@ -13,7 +14,9 @@ describe('ScraperController', () => {
     controller = app.get<ScraperController>(ScraperController);
   });
 
-  it('should be defined', () => {
-    expect(controller.v1()).toBeDefined();
+  it('should fetch a company by companyId', () => {
+    expect(controller.byCompanyId({ country: 'CH', companyId: '123' })).toEqual(
+      { country: 'CH', companyId: '123' },
+    );
   });
 });
