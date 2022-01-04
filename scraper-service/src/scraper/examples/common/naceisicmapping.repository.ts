0b3e31d9ddfbfcdb48eea769cdc8f7e3f00d0811
Anders Borch <anders@cyborch.com>
2022-01-04 11:22:10 +0100

    Added nace to isic mapping repository

--- scraper-service/src/scraper/examples/common/naceisicmapping.repository.ts
+++
@@ -1,7 +0,0 @@
-import { NaceIsicMapping } from './naceisicmapping.model';
-
-export class NaceIsicMappingRepository {
-  async findByNace(nace: string): Promise<NaceIsicMapping> {
-    return new NaceIsicMapping();
-  }
-}
